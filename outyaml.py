114c7b531285f0649398a67a233196a3e0c891f3
Emil Eriksson <emil.eriksson@codemill.se>
2015-08-17 16:07:52 +0200

    Correctly(?) calculate estimates

--- outyaml.py
+++ outyaml.py
@@ -1,7 +1,127 @@
-#!/usr/bin/env python
-
+#!/Users/emieri/Documents/Projects/outyaml/virtualenvs/outyaml/bin/python
 import yaml
 import sys
+import re
+
+from collections import OrderedDict
+
+
+class Duration:
+    UNITS = OrderedDict((
+        ("d", float(24 * 60 * 60)),
+        ("h", float(60 * 60)),
+        ("m", float(60)),
+        ("s", float(1)),
+    ))
+
+    parse_re = re.compile(r"(?:\s*(?:\d+\.\d*[dhmsp]))*\s*")
+
+    @classmethod
+    def from_string(cls, string):
+        duration = 0.0
+        points = 0.0
+        for fragment in string.split():
+            unit = fragment[-1]
+            value = float(fragment[:-1])
+            if unit in Duration.UNITS:
+                duration += value * Duration.UNITS[unit]
+            elif "p" == unit:
+                points += value
+
+        return cls(duration=duration, points=points)
+
+    def __init__(self, duration=0, points=0):
+        self.duration = float(duration)
+        self.points = float(points)
+
+    def __str__(self):
+        result = []
+        if 0 < self.points:
+            result.append("%.1fp" % self.points)
+        duration = self.duration
+        for unit, size in Duration.UNITS.items():
+            if duration >= size:
+                result.append("%d%s" % (int(duration / size), unit))
+                duration = int(duration % size)
+
+        return " ".join(result)
+
+    def __bool__(self):
+        return self.duration > 0 or self.points > 0
+
+    def __add__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented()
+
+        result = self.__class__(
+            duration=self.duration,
+            points=self.points,
+        )
+
+        result.duration += other.duration
+        result.points += other.points
+
+        return result
+
+
+class Estimate:
+    #parser_re = re.compile(r"(?:\s*([+~-]?)(\d+[wdhmsp]))*")
+    parser_re = re.compile(r"(?:[+~-]\((?=[^)]*\)))?(?:\d+\.?\d*[dhmsp]\s*)+\)?")
+
+    @classmethod
+    def from_string(cls, string):
+        estimate = Estimate()
+        for match in cls.parser_re.finditer(string):
+            fragment = match.group(0)
+            if "+" == fragment[0]:
+                estimate.over = Duration.from_string(fragment[2:-1])
+            elif "-" == fragment[0]:
+                estimate.under = Duration.from_string(fragment[2:-1])
+            elif "~" == fragment[0]:
+                estimate.fudge = Duration.from_string(fragment[2:-1])
+            else:
+                estimate.duration = Duration.from_string(fragment)
+        return estimate
+
+    def __init__(self):
+        self.duration = Duration()
+        self.over = Duration()
+        self.under = Duration()
+        self.fudge = Duration()
+
+    def __str__(self):
+        result = []
+
+        if self.duration:
+            result.append(str(self.duration))
+
+        if self.under:
+            result.append("-(%s)" % (self.under))
+
+        if self.over:
+            result.append("+(%s)" % (self.over))
+
+        if self.fudge:
+            result.append("~(%s)" % (self.fudge))
+
+        return " ".join(result)
+
+    def __bool__(self):
+        return bool(self.duration or self.under or
+                self.over or self.fudge)
+
+    def __add__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented()
+
+        result = self.__class__()
+
+        result.duration = self.duration + other.duration
+        result.over = self.over + other.over
+        result.under = self.under + other.under
+        result.fudge = self.fudge + other.fudge
+
+        return result
 
 
 class Tag:
@@ -72,6 +192,16 @@ class tr(Tag):
     name = "tr"
 class td(Tag):
     name = "td"
+class section(Tag):
+    name = "section"
+class p(Tag):
+    name = "p"
+class ul(Tag):
+    name = "ul"
+class li(Tag):
+    name = "li"
+class h6(Tag):
+    name = "h6"
 
 
 def render_outline(outline, settings):
@@ -187,6 +317,8 @@ def render_style(settings):
 
 
 def render_outlines(documents):
+    settings = {}
+    outlines = []
     for document in documents:
         if ("columns" in document and "rows" in document and
             "levels" in document):
@@ -194,8 +326,6 @@ def render_outlines(documents):
         else:
             outlines.append(document)
 
-
-    # print(render_style(settings))
 
     content = body()
 
@@ -246,13 +376,87 @@ def render_outlines(documents):
 
     return html_document
 
+
+def render_reports(documents):
+    for document in documents:
+        if type(document) is list:
+            items = render_report_items(document, {}, level=3)
+            for item in items:
+                yield item
+
+
+def render_report_items(document, settings, level=1):
+    class header(Tag):
+        name = "h%d" % level
+
+    def _render_item(title, attributes):
+        result = []
+        h = header()
+        h.append(title)
+        result.append(h)
+
+        if type(attributes) is dict:
+            estimate = Estimate()
+            if "tasks" in attributes:
+                estimate = sum_estimate(attributes["tasks"], key="estimate", subkey="tasks")
+            if not(estimate) and "estimate" in attributes:
+                estimate = Estimate.from_string(attributes["estimate"])
+
+            if estimate:
+                estimate_element = div(class_="estimate")
+                estimate_element.append("&nbsp;")
+                estimate_element.append(str(estimate))
+                estimate_element.append_to(h)
+
+            if "description" in attributes:
+                description = p(class_="description")
+                description.append(attributes["description"])
+                description
+
+                result.append(description)
+
+            if False and "notes" in attributes:
+                notes = attributes["notes"]
+                result.append(h6().append("Notes"))
+                if type(notes) is list:
+                    l = ul(class_="notes")
+                    for item in notes:
+                        li().append(item).append_to(l)
+                    result.append(l)
+                else:
+                    result.append(p(class_="notes").append(notes))
+
+            if "tasks" in attributes and attributes["tasks"]:
+                result.extend(render_report_items(attributes["tasks"],
+                                                  settings, level+1))
+
+        return "".join(map(str, result))
+
+    for epic in document:
+        for title, attributes in epic.items():
+            yield _render_item(title, attributes)
+
+
+def sum_estimate(document, key="estimate", subkey="tasks"):
+    estimates = []
+    for epic in document:
+        for title, attributes in epic.items():
+            if type(attributes) is dict:
+                sub_estimate = sum_estimate(attributes.get(subkey, []))
+                if sub_estimate:
+                    estimates.append(sub_estimate)
+                elif key in attributes:
+                    string = attributes[key]
+                    estimates.append(Estimate.from_string(string))
+
+    return sum(estimates, Estimate())
+
+
 if "__main__" == __name__:
     documents = yaml.load_all(sys.stdin.read())
-    settings = {}
-    outlines = []
 
-
-
-
+    html_document = "".join(map(str, render_reports(documents)))
+    # for document in documents:
+    #     print(sum_estimate(document, key="estimate"))
 
     print(html_document)
